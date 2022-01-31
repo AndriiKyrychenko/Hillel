0aab5f0486985d8f4dacf3d2af3ebe2ebe0b74f4
AndriiKyrychenko <97938375+AndriiKyrychenko@users.noreply.github.com>
2022-01-31 12:58:30 +0200

    Final work, end of Introduction Java course

---
+++ src/models/Product.java
@@ -0,0 +1,51 @@
+package models;
+
+
+import base.Fiscal;
+import base.Income;
+
+// Model.
+
+public class Product implements Income, Fiscal {
+
+    private String name;
+    private int quantity;
+    private double price;
+
+    public final double TAX = 0.05;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    //GETTERS
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public double getTotalIncome() {
+        double totalIncome = quantity * price;
+        return totalIncome;
+    }
+
+    @Override
+    public double getTax() {
+        double tax = getTotalIncome() * TAX;
+        return tax;
+    }
+
+    @Override
+    public double getNetIncome() {
+        double netIncome = getTotalIncome() - getTax();
+        return netIncome;
+    }
+}
---
+++ src/views/SalesView.java
@@ -0,0 +1,53 @@
+package views;
+
+
+import models.Product;
+import utils.Validator;
+
+import java.util.Scanner;
+
+// View
+public class SalesView {
+
+    String title;
+    String name;
+    int quantity;
+    double price;
+
+    Scanner scanner;
+
+    Product model;
+
+    public SalesView(Product model) {
+        this.model = model;
+    }
+
+    Validator validator = new Validator();
+
+    public void getInputs() {
+
+        scanner = new Scanner(System.in);
+
+        title = "Введите наименование товара: ";
+        System.out.println(title);
+        name = validator.validateName(scanner);
+        model.setName(name);
+
+        title = "Введите количество: ";
+        System.out.println(title);
+        quantity = validator.validateQuantityInput(scanner);
+        model.setQuantity(quantity);
+
+        title = "Введите цену: ";
+        System.out.println(title);
+        price = validator.validatePriceInput(scanner);
+        model.setPrice(price);
+
+        scanner.close();
+    }
+
+    public void getOutput(String output) {
+
+        System.out.println(output);
+    }
+}
